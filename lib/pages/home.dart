75588b589f358e86861ec127ac0c04f6c50add21
Aniket Kale <aniketkale199200@gmail.com>
2022-02-28 12:06:22 +0530

    dish change

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -69,7 +69,7 @@ class _HomePageState extends State<HomePage> {
                               onTap: () {
                                 setState(
                                   () {
-                                    dish = "chicken";
+                                    dish = "ladoo";
                                   },
                                 );
                               },
